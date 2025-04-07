c74251faa0f3a03883e15f7ae2f8d57ce4cd7927
JDR69 <joandanielrr@gmail.com>
2025-04-07 19:05:37 -0400

    Bienvenido

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import LoginPage from "./pages/loginPage"
+
+function App() {
+
+  return (
+    <BrowserRouter>
+      <Main/>
+    </BrowserRouter>
+  )
+}
+
+function Main() {
+  return(
+    <>
+     
+      <Routes>
+        <Route path="/login" element={<LoginPage/>}/>
+      </Routes>
+    </>
+  )
+}
+export default App
+
